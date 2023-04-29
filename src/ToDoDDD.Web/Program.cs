dae53cd197c37758637605404f898b55a19545c6
Pe6e3 <xpe6e3@gmail.com>
2023-04-29 12:04:36 +0600

    Прописал связь с БД

--- src/ToDoDDD.Web/Program.cs
+++ src/ToDoDDD.Web/Program.cs
@@ -1,7 +1,11 @@
+using ToDoDDD.DAL.Datas;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("AppDbContext")
 
 var app = builder.Build();
 
