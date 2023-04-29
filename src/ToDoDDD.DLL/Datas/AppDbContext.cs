cfd73340b2811f42438a4c188a2cd436e73b1cca
Pe6e3 <xpe6e3@gmail.com>
2023-04-29 11:07:22 +0600

    Создал Контекст БД

---
+++ src/ToDoDDD.DLL/Datas/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoDDD.DAL.Entities;
+
+namespace ToDoDDD.DAL.Datas;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    { }
+
+    public DbSet<Taska> Tasks { get; set; }
+    public DbSet<Status> Statuses { get; set; }
+    public DbSet<Prioritet> Priorites { get; set; }
+
+}
