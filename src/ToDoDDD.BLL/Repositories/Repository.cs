6290ff7438cb4b5df8de5ae4f8e6183469e3fe91
Pe6e3 <xpe6e3@gmail.com>
2023-04-29 11:07:07 +0600

    Создал репозиторий и его интерфейс

---
+++ src/ToDoDDD.BLL/Repositories/Repository.cs
@@ -0,0 +1,41 @@
+﻿using ToDoDDD.DAL.Datas;
+using Microsoft.EntityFrameworkCore;
+using ToDoDDD.DAL.Interfaces;
+
+namespace ToDoDDD.BLL.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    private readonly AppDbContext _db;
+    private readonly DbSet<T> dbSet;
+    public Repository(AppDbContext db)
+    {
+        _db = db;
+        dbSet = db.Set<T>();
+    }
+
+    public void Delete(T entity)
+    {
+        if (_db.Entry(entity).State == EntityState.Detached)
+        {
+            dbSet.Attach(entity);
+        }
+        dbSet.Remove(entity);
+    }
+
+    public IEnumerable<T> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T GetByGuid(Guid guid)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(T entity)
+    {
+        dbSet.Attach(entity);
+        _db.Entry(entity).State = EntityState.Modified;
+    }
+}
---
+++ src/ToDoDDD.DLL/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoDDD.DAL.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    IEnumerable<T> Get();
+    T GetByGuid(Guid guid);
+    void Delete(T entity);
+    void Update(T entity);
+
+}
